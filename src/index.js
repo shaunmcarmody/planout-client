9ad42f6fe33cabd5f2489f69069339bc79286496
shaunmcarmody <shaun.carmody@live.co.uk>
2019-12-15 18:36:14 +0000

    add logger

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './store/reducers';
 import App from './App';
 import './index.css';
 
-const store = createStore(applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>

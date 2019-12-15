db6f362b472ae859c11574717d09ab88c2c26228
shaunmcarmody <shaun.carmody@live.co.uk>
2019-12-15 18:40:14 +0000

    use default action to get link when component mounts

--- src/views/LearnReact/LearnReact.js
+++ src/views/LearnReact/LearnReact.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getLink } from '../../store/actions/defaultActions';
 import logo from '../../assets/logo.svg';
 import './LearnReact.css';
 
-const LearnReact = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-)
+class LearnReact extends Component {
+  componentDidMount() {
+    this.props.getLink()
+  }
 
-export default LearnReact;
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.props.link}
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  link: state.defaultReducer.link
+})
+
+export default connect(mapStateToProps, { getLink })(LearnReact);
